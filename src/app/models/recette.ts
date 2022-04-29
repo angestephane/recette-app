eeadc6c7c8a07572285389078e6a1e6650ffae8f
stephane boguhe <boguhestephane97@gmail.com>
2022-04-29 16:12:51 +0000

    ajout d'un modèle

---
+++ src/app/models/recette.ts
@@ -0,0 +1,8 @@
+export class Recette {
+  reference : number | undefined;
+  deignation : string | undefined;
+  description :  string | undefined;
+  ingredient : Array<string> | undefined;
+  level : number | undefined;
+  tempsCuisson : number | undefined;
+}
